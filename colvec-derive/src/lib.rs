2ceb75adcb078057d00d6521869637941fb55f0b
Rhys Lloyd <krakow20@gmail.com>
2025-08-20 16:13:27 -0700

    initial macro code structure

--- colvec-derive/src/lib.rs
+++ colvec-derive/src/lib.rs
@@ -1,36 +1,29 @@
 use proc_macro::TokenStream;
-use quote::{quote, ToTokens};
+use quote::quote;
 use syn::{parse_macro_input,DeriveInput};
 
 #[proc_macro_derive(ColVec)]
 pub fn colvec_derive(input:TokenStream)->TokenStream{
-	let input: DeriveInput=parse_macro_input!(input);
+	let input:DeriveInput=parse_macro_input!(input);
 
-	let ident=input.ident;
+	match input.data{
+		syn::Data::Struct(syn::DataStruct{fields:syn::Fields::Named(fields_named),..})=>derive_struct(input.ident,input.vis,fields_named),
+		_=>unimplemented!("Only structs are supported"),
+	}
+}
 
-	let ident_str={
-		let mut ident=ident.to_string();
-		ident.push_str("ColVec");
-		ident
+fn derive_struct(ident:syn::Ident,vis:syn::Visibility,fields:syn::FieldsNamed)->TokenStream{
+	let colvec_ident_string=format!("{ident}ColVec");
+	let colvec_ident=syn::Ident::new(&colvec_ident_string,ident.span());
+
+	let colvec = quote!{
+		#vis struct #colvec_ident<A: ::colvec::alloc::Allocator = ::colvec::alloc::Global>{
+			buf: ::colvec::raw::RawColVec<#ident, A>,
+			len: usize,
+		}
 	};
 
-	let ident=syn::Ident::new(&ident_str,ident.span());
-
-	// colvec::colvec!();
-	let props:[syn::Field;0]=[];
-
-	let mut colvec=syn::ItemStruct{
-		attrs: Vec::new(),
-		vis: syn::Visibility::Public(syn::token::Pub::default()),
-		struct_token: syn::token::Struct::default(),
-		ident,
-		generics: input.generics.into(),
-		fields: syn::Fields::Named(syn::FieldsNamed {
-			brace_token: syn::token::Brace::default(),
-			named: props.into_iter().collect(),
-		}),
-		semi_token: None,
-	};
-
-	colvec.into_token_stream().into()
+	quote! {
+		#colvec
+	}.into()
 }
