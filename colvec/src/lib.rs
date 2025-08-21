75084c882d3842f616720c335196bafc8333f4ff
Rhys Lloyd <krakow20@gmail.com>
2025-08-20 18:08:36 -0700

    main?

--- colvec/src/lib.rs
+++ colvec/src/lib.rs
@@ -11,25 +11,3 @@ pub mod alloc;
 pub mod fields;
 #[doc(hidden)]
 pub mod raw;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-	#[derive(ColVec)]
-	struct Test{
-		field1:u8,
-		field2:i32,
-	}
-
-	#[test]
-	fn it_works() {
-		// let mut test=TestColVec::new();
-		// test.push(Test{
-		// 	field1:1,
-		// 	field2:-1,
-		// });
-		// assert_eq!( 1, test.field1_slice()[0]);
-		// assert_eq!(-1, test.field2_slice()[0]);
-	}
-}
