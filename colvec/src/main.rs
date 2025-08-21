43cbbb51239cf7187e4213e54b8deb2d6210c472
Rhys Lloyd <krakow20@gmail.com>
2025-08-20 18:54:55 -0700

    smoke test

--- colvec/src/main.rs
+++ colvec/src/main.rs
@@ -4,19 +4,23 @@ fn main(){}
 mod tests {
     use ::colvec::*;
 
-	#[derive(ColVec)]
-	struct Test{
-		field0:u8,
-		field1:i32,
-	}
-
 	#[test]
-	fn it_works() {
+	fn smoke_test_macro() {
+		#[derive(ColVec)]
+		struct Test{
+			field0:u8,
+			field1:i32,
+		}
+
 		let mut test=TestColVec::new();
 		test.push(Test{
 			field0:1,
 			field1:-1,
 		});
+
+		test.field0_slice_mut().sort();
+		test.field1_slice_mut().sort();
+
 		assert_eq!( 1, test.field0_slice()[0]);
 		assert_eq!(-1, test.field1_slice()[0]);
 	}
