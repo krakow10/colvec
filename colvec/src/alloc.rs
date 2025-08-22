661b9dabaa2ef0bac8433563cd926e393064bd21
Rhys Lloyd <krakow20@gmail.com>
2025-08-22 16:20:01 -0700

    fix it all using mutually exclusive positive features

--- colvec/src/alloc.rs
+++ colvec/src/alloc.rs
@@ -1,9 +1,9 @@
-#[cfg(not(feature = "nightly"))]
+#[cfg(feature = "allocator-api2")]
 pub use allocator_api2::alloc::Allocator;
 #[cfg(feature = "nightly")]
 pub use core::alloc::Allocator;
 
-#[cfg(all(not(feature = "nightly"),feature = "std"))]
+#[cfg(all(feature = "allocator-api2",feature = "std"))]
 pub use allocator_api2::alloc::Global;
 #[cfg(all(feature = "nightly",feature = "std"))]
 pub use std::alloc::Global;
--- colvec/src/lib.rs
+++ colvec/src/lib.rs
@@ -1,6 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(feature = "nightly", feature(allocator_api))]
 
+#[cfg(all(feature = "allocator-api2", feature = "nightly"))]
+compile_error!("feature \"allocator-api2\" and feature \"nightly\" cannot be enabled at the same time");
+
 pub use colvec_derive::ColVec;
 
 mod error;
