47b0af2535d1e9a882b879383b9a005d0e13ff7d
Rhys Lloyd <krakow20@gmail.com>
2025-08-22 17:07:46 -0700

    oh

--- colvec/src/alloc.rs
+++ colvec/src/alloc.rs
@@ -1,4 +1,9 @@
 #[cfg(feature = "allocator-api2")]
-pub use allocator_api2::alloc::{Allocator,Global};
+pub use allocator_api2::alloc::{handle_alloc_error,Allocator};
 #[cfg(feature = "nightly")]
-pub use core::alloc::{Allocator,Global};
+pub use core::alloc::{handle_alloc_error,Allocator};
+
+#[cfg(all(feature = "allocator-api2",feature = "std"))]
+pub use allocator_api2::alloc::Global;
+#[cfg(all(feature = "nightly",feature = "std"))]
+pub use std::alloc::Global;
