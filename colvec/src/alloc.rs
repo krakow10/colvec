3bcf216b4a4bbc407926b011993fd1ce71fa6b30
Rhys Lloyd <krakow20@gmail.com>
2025-08-22 18:31:09 -0700

    import everything

--- colvec/src/alloc.rs
+++ colvec/src/alloc.rs
@@ -1,7 +1,7 @@
 #[cfg(feature = "allocator-api2")]
-pub use allocator_api2::alloc::Allocator;
+pub use allocator_api2::alloc::*;
 #[cfg(feature = "nightly")]
-pub use core::alloc::Allocator;
+pub use core::alloc::*;
 
 #[cfg(all(feature = "allocator-api2",feature = "std"))]
 pub use allocator_api2::alloc::Global;
